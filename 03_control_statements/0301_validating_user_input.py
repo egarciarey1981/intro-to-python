73337148cf1b72d97e0b0f584bdd460585d195d7
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-04 20:31:30 +0100

    Validating User Input

--- 03_control_statements/0301_validating_user_input.py
+++ 03_control_statements/0301_validating_user_input.py
@@ -19,11 +19,11 @@ while student < 10:
     result = int(input('Enter result (1=pass, 2=fail): '))
 
     if result == 1:
-        passes = passes + 1
-        student = student + 1
+        passes += 1
+        student += 1
     elif result == 2:
-        failures = failures + 1
-        student = student + 1
+        failures += 1
+        student += 1
 
 print('Passed:', passes)
 print('Failed:', failures)
