c263cd6e54fd423eeb5c3b62d6f75e96c6e0a71a
Eliecer Garcia Rey <erey@freepik.com>
2023-01-13 13:46:50 +0100

    Arbitrary Argument List

---
+++ 04_functions/0413_product.py
@@ -0,0 +1,29 @@
+###############################################################################
+#                                Exercice 2.13                                #
+###############################################################################
+#                                                                             #
+# Calculate the product of a series of integers that passed to the function   #
+# product, which receives an arbitrary argument list. Test your function with #
+# several calls, each with a different number of arguments.                   #
+#                                                                             #
+###############################################################################
+
+
+def product(*integers):
+    total = 1
+    for integer in integers:
+        total *= integer
+    return total
+
+list1 = [1,2,3]
+list2 = [1,2,3,4]
+
+print()
+print(list1)
+print('product(list1): ', product(*list1))
+
+print()
+print(list2)
+print('product(list2): ', product(*list2))
+
+print()
