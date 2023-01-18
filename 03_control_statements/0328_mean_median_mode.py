f6b4e65452203d399cb2ac7d743b2015462d9504
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 20:11:33 +0100

    Intro to Data Science: Mean, Median and Mode

--- 03_control_statements/0328_mean_median_mode.py
+++ 03_control_statements/0328_mean_median_mode.py
@@ -1,15 +1,11 @@
-###############################################################################
-#                                Exercice 3.28                                #
-###############################################################################
-#                                                                             #
-# Calculate the mean, median and mode of the values 9,11,22,34,17,22,34,22    #
-# and 40. Suppose the values included another 34. What problem might occur?   #
-#                                                                             #
-###############################################################################
+'''
+Calculate the mean, median and mode of the values 9, 11, 22, 34, 17, 22, 34, 22
+and 40. Suppose the values included another 34. What problem might occur?
+'''
 
 import statistics
 
-values = [9,11,22,34,17,22,34,22]
+values = [9, 11, 22, 34, 17, 22, 34, 22]
 print()
 print(values)
 print('mean  :', statistics.mean(values))
