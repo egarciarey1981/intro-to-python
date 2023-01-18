db8ef9fdd0ef061b9c7b492cec573560603f0621
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 19:46:54 +0100

    Table of Squares and Cubes

--- 03_control_statements/0307_squares_cubes.py
+++ 03_control_statements/0307_squares_cubes.py
@@ -1,22 +1,18 @@
-###############################################################################
-#                                Exercice 3.7                                 #
-###############################################################################
-#                                                                             #
-# In Exercise 2.8, you wrote a script to calculate the squares and cubes of   #
-# the numbers from 0 through 5, then printed the resulting values in table    #
-# format. Reimplement your script using a for loop and the f-string           #
-# capabilities you learned in this chapter to produce the following table     #
-# with the numbers right aligned in each column.                              #
-#                                                                             #
-#   number  square  cube                                                      #
-#       0       0     0                                                       #
-#       1       1     1                                                       #
-#       2       4     8                                                       #
-#       3       9    27                                                       #
-#       4      16    64                                                       #
-#       5      25   125                                                       #
-#                                                                             #
-###############################################################################
+'''
+In Exercise 2.8, you wrote a script to calculate the squares and cubes of
+the numbers from 0 through 5, then printed the resulting values in table
+format. Reimplement your script using a for loop and the f-string
+capabilities you learned in this chapter to produce the following table
+with the numbers right aligned in each column.
+
+  number  square    cube                                                      
+       0       0       0
+       1       1       1
+       2       4       8
+       3       9      27
+       4      16      64
+       5      25     125
+'''                                                                            
 
 spaces = 6
 
