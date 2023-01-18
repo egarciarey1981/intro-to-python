913d031737a287bfd68e0658b63a349f64f009f7
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 19:55:01 +0100

    Nested Loops

--- 03_control_statements/0317_nested_loops.py
+++ 03_control_statements/0317_nested_loops.py
@@ -1,9 +1,13 @@
 '''
-Write a script that displays the following triangle patterns separately, one below the other. Separate each pattern from the next by one blank line. Use for loops to generate the patterns. Display all asterisks (*) with a single statement of the form
+Write a script that displays the following triangle patterns separately, one
+below the other. Separate each pattern from the next by one blank line. Use for
+loops to generate the patterns. Display all asterisks (*) with a single
+statement of the form
 
     print('*', end='')                                                   
 
-which causes the asterisks to display side by side. [Hint: For the last two patterns, begin each line with zero or more space characters.]
+which causes the asterisks to display side by side. [Hint: For the last two
+patterns, begin each line with zero or more space characters.]
 
    (a)           (b)           (c)           (d)
    *             **********    **********             *
