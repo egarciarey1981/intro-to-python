77bbd08558c0d45a1c90e9253300a237d23f5e68
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 20:23:18 +0100

    IPython Session: Slicing

--- 05_squences/0505_slicing.py
+++ 05_squences/0505_slicing.py
@@ -1,18 +1,14 @@
-###############################################################################
-#                                Exercice 5.5                                 #
-###############################################################################
-#                                                                             #
-# Create a string called alphabet containing 'abcdefghijklmnopqrstuvwxyz',    #
-# then perform the following separate slice operations to obtain:             #
-#   a) The first half of the string using starting and ending indices.        #
-#   b) The first half of the string using only the ending index.              #
-#   c) The second half of the string using starting and ending indices.       #
-#   d) The second half of the string using only the starting index.           #
-#   e) Every second letter in the string starting with 'a'.                   #
-#   f) The entire string in reverse.                                          #
-#   g) Every third letter of the string in reverse starting with 'z'.         #                                           
-#                                                                             #
-###############################################################################
+'''
+Create a string called alphabet containing 'abcdefghijklmnopqrstuvwxyz', then
+perform the following separate slice operations to obtain:
+    a) The first half of the string using starting and ending indices.
+    b) The first half of the string using only the ending index.
+    c) The second half of the string using starting and ending indices.
+    d) The second half of the string using only the starting index.
+    e) Every second letter in the string starting with 'a'.
+    f) The entire string in reverse.
+    g) Every third letter of the string in reverse starting with 'z'.
+'''
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
