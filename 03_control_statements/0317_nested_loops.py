9165d70e5add14735be76d26d0b29086e209b06f
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 19:53:47 +0100

    Nested Loops

--- 03_control_statements/0317_nested_loops.py
+++ 03_control_statements/0317_nested_loops.py
@@ -1,30 +1,22 @@
-###############################################################################
-#                                Exercice 3.17                                #
-###############################################################################
-#                                                                             #
-# Write a script that displays the following triangle patterns separately,    #
-# one below the other. Separate each pattern from the next by one blank line. #
-# Use for loops to generate the patterns. Display all asterisks (*) with a    #
-# single statement of the form                                                #
-#                                                                             #
-#       print('*', end='')                                                    #
-#                                                                             #
-# which causes the asterisks to display side by side. [Hint: For the last two #
-# patterns, begin each line with zero or more space characters.]              #
-#                                                                             #
-#    (a)           (b)           (c)           (d)                            #
-#    *             **********    **********             *                     #
-#    **            *********      *********            **                     #
-#    ***           ********        ********           ***                     #
-#    ****          *******          *******          ****                     #
-#    *****         ******            ******         *****                     #
-#    ******        *****              *****        ******                     #
-#    *******       ****                ****       *******                     #
-#    ********      ***                  ***      ********                     #
-#    *********     **                    **     *********                     #
-#    **********    *                      *    **********                     #
-#                                                                             #
-###############################################################################
+'''
+Write a script that displays the following triangle patterns separately, one below the other. Separate each pattern from the next by one blank line. Use for loops to generate the patterns. Display all asterisks (*) with a single statement of the form
+
+    print('*', end='')                                                   
+
+which causes the asterisks to display side by side. [Hint: For the last two patterns, begin each line with zero or more space characters.]
+
+   (a)           (b)           (c)           (d)
+   *             **********    **********             *
+   **            *********      *********            **
+   ***           ********        ********           ***
+   ****          *******          *******          ****
+   *****         ******            ******         *****
+   ******        *****              *****        ******
+   *******       ****                ****       *******
+   ********      ***                  ***      ********
+   *********     **                    **     *********
+   **********    *                      *    **********
+'''
 
 print('(a)')
 
