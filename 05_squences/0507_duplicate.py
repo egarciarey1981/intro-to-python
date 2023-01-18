62a3bb4e44b3e390199c3946645990acdb202e45
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 20:24:43 +0100

    Duplicate Elimination

--- 05_squences/0507_duplicate.py
+++ 05_squences/0507_duplicate.py
@@ -1,17 +1,15 @@
-###############################################################################
-#                                Exercice 5.7                                 #
-###############################################################################
-#                                                                             #
-# Create a function that receives a list and returns a (possibly shorter)     #
-# list containing only the unique values in sorted order. Test your function  #
-# with a list of numbers and a list of strings.                               #
-#                                                                             #
-###############################################################################
+'''
+Create a function that receives a list and returns a (possibly shorter) list
+containing only the unique values in sorted order. Test your function with a
+list of numbers and a list of strings.
+'''
+
 
 def removeDuplicates(elements):
     return [elements[i] for i in range(len(elements)) if elements[i] not in elements[0:i]]
 
-numbers = [1,2,2,3,3,3,1,2,2,3,3,3]
+
+numbers = [1, 2, 2, 3, 3, 3, 1, 2, 2, 3, 3, 3]
 uniques = removeDuplicates(numbers)
 
 print(numbers)
