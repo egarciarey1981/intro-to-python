bd821813296d806738dbe3b17d3905363ae8466f
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 19:58:26 +0100

    Challenge: Nested Looping

--- 03_control_statements/0318_challenge_nested_loops.py
+++ 03_control_statements/0318_challenge_nested_loops.py
@@ -1,37 +1,30 @@
-###############################################################################
-#                                Exercice 3.18                                #
-###############################################################################
-#                                                                             #
-# Modify your script from Exercise 3.17 to display all four patterns          #
-# side-by-side (as shown above) by making clever use of nested for loops.     #
-# Separate each triangle from the next by three horizontal spaces. [Hint: One #
-# for loop should control the row number. Its nested for loops should         #
-# calculate from the row number the appropriate number of asterisks and       #
-# spaces for each of the four patterns.]                                      #
-#                                                                             #
-###############################################################################
+'''
+Modify your script from Exercise 3.17 to display all four patterns side-by-side
+(as shown above) by making clever use of nested for loops Separate each triangle
+from the next by three horizontal spaces. [Hint: One for loop should control the
+row number. Its nested for loops should calculate from the row number the
+appropriate number of asterisks and spaces for each of the four
+patterns.]
+'''
 
 print()
 print('(a)', '(b)', '(c)', '(d)', sep='          ')
 
-for i in range(1,11):
+for i in range(1, 11):
     for j in range(i):
         print('*', end='')
     for j in range(10-i+3):
         print(' ', end='')
-
 
     for j in range(11-i):
         print('*', end='')
     for j in range(i-1+3):
         print(' ', end='')
 
-
     for j in range(i-1):
         print(' ', end='')
     for j in range(11-i):
         print('*', end='')
-
 
     for j in range(10-i+3):
         print(' ', end='')
