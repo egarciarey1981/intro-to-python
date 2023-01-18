17f7cacb413bf10b470137ae9e707ca818542577
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 20:42:15 +0100

    Is a Sequence Sorted?

--- 05_squences/0514_is_ordered.py
+++ 05_squences/0514_is_ordered.py
@@ -1,12 +1,9 @@
-###############################################################################
-#                                Exercice 5.14                                #
-###############################################################################
-#                                                                             #
-# Create a function is_ordered that receives a sequence and returns True if   #
-# the elements are in sorted order. Test your function with sorted and        #
-# unsorted lists, tuples and strings.                                         #
-#                                                                             #
-###############################################################################
+'''
+Create a function is_ordered that receives a sequence and returns True if the
+elements are in sorted order. Test your function with sorted and unsorted lists,
+tuples and strings.
+'''
+
 
 def is_ordered(squence):
     for i in range(len(squence)-1):
@@ -21,10 +18,10 @@ print(is_ordered(list), list)
 list = ["red", "orange", "yellow"]
 print(is_ordered(list), list)
 
-tuple = (1,2,3)
+tuple = (1, 2, 3)
 print(is_ordered(tuple), tuple)
 
-tuple = (1,3,2)
+tuple = (1, 3, 2)
 print(is_ordered(tuple), tuple)
 
 string = "abcd"
