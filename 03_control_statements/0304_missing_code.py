0fd159c2f7728a9f34060a2869dcfd91dfc5e8de
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 19:37:53 +0100

    Fill in the Missing Code

--- 03_control_statements/0304_missing_code.py
+++ 03_control_statements/0304_missing_code.py
@@ -1,21 +1,17 @@
-###############################################################################
-#                                Exercice 3.4                                 #
-###############################################################################
-#                                                                             #
-# In the code below                                                           #
-#                                                                             #
-#   for ***:                                                                  #
-#       for ***:                                                              #
-#           print('@')                                                        #
-#       print()                                                               #
-#                                                                             #
-# replace the *** so that when you execute the code, it displays two rows,    #
-# each containing seven @ symbols, as in:                                     #
-#                                                                             #
-#   @@@@@@@                                                                   #
-#   @@@@@@@                                                                   #
-#                                                                             #
-###############################################################################
+'''
+In the code below
+
+    for ***:
+        for ***:
+            print('@')
+        print()
+
+replace the *** so that when you execute the code, it displays two rows, each
+containing seven @ symbols, as in:
+
+    @@@@@@@
+    @@@@@@@
+'''
 
 for row in range(2):
     for column in range(7):
