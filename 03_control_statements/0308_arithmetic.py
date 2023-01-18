c3495c2af475ab377a81219bd718672e59dffdac
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 19:49:14 +0100

    Arithmetic, Smallest and Largest

--- 03_control_statements/0308_arithmetic.py
+++ 03_control_statements/0308_arithmetic.py
@@ -1,12 +1,8 @@
-###############################################################################
-#                                Exercice 3.8                                 #
-###############################################################################
-#                                                                             #
-# In Exercise 2.10, you wrote a script that input three integers, then        #
-# displayed the sum, average, product, smallest and largest of those values.  #
-# Reimplement your script with a loop that inputs four integers.              #
-#                                                                             #
-###############################################################################
+'''
+In Exercise 2.10, you wrote a script that input three integers, then
+displayed the sum, average, product, smallest and largest of those values.
+Reimplement your script with a loop that inputs four integers.
+'''
 
 count = 4
 
@@ -19,14 +15,12 @@ for number in range(count):
     x = int(input('number '))
     sum += x
     product *= x
-    smallest = min(smallest,x)
-    largest = max(largest,x)
-    
+    smallest = min(smallest, x)
+    largest = max(largest, x)
+
 print()
 print('sum     :', sum)
 print('average :', sum / count)
 print('product :', product)
 print('smallest:', smallest)
 print('largest :', largest)
-
-
