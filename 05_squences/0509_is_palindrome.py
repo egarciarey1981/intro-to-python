1aabc3e5de2dd15da429fe3f9d89d6dea8193c93
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 20:39:21 +0100

    Palindrome Tester

--- 05_squences/0509_is_palindrome.py
+++ 05_squences/0509_is_palindrome.py
@@ -1,32 +1,25 @@
-###############################################################################
-#                                Exercice 5.9                                 #
-###############################################################################
-#                                                                             #
-# A string that’s spelled identically backward and forward, like 'radar', is  #
-# a palindrome. Write a function is_palindrome that takes a string and        #
-# returns True if it’s a palindrome and False otherwise. Use a stack          #
-# (simulated with a list as we did in Section 5.11) to help determine whether #
-# a string is a palindrome. Your function should ignore case sensitivity      #
-# (that is, 'a' and 'A' are the same), spaces and punctuation.                #
-#                                                                             #
-###############################################################################
+'''
+A string that's spelled identically backward and forward, like 'radar', is a
+palindrome. Write a function is_palindrome that takes a string and returns True
+if it's a palindrome and False otherwise. Use a stack (simulated with a list as
+we did in Section 5.11) to help determine whether a string is a palindrome. Your
+function should ignore case sensitivity (that is, 'a' and 'A' are the same),
+spaces and punctuation.
+'''
+
 
 def is_palindrome(word):
-
-    word = word.lower()
-    wordClear = ""
-    for index in range(len(word)):
-        if word[index] not in [' ', '.', ',']:
-            wordClear += word[index]
-
+    # clear word
+    word = [x for x in word.lower() if x not in [' ', '.', ',']]
+    # put word in the stack
     stack = []
-    for index in range(len(wordClear)):
-        stack.append(wordClear[index])
-
-    for index in range(len(wordClear)):
-        if wordClear[index] != stack.pop():
+    for i in range(len(word)):
+        stack.append(word[i])
+    # compare letters
+    for i in range(len(word)):
+        if word[i] != stack.pop():
             return False
-
+    # if you are here, is True
     return True
 
 
