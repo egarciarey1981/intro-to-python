dd6e3031ab6916c2b846fa9a3e58e2c9dba0d021
Eliecer García Rey <egarciarey1981@gmail.com>
2023-01-18 20:09:18 +0100

    Optional else Clause of a Loop

--- 03_control_statements/0322_loop_with_else.py
+++ 03_control_statements/0322_loop_with_else.py
@@ -1,24 +1,20 @@
-###############################################################################
-#                                Exercice 3.22                                #
-###############################################################################
-#                                                                             #
-# The while and for statements each have an optional else clause. In a while  #
-# statement, the else clause executes when the condition becomes False. In a  #
-# for statement, the else clause executes when there are no more items to     #
-# process. If you break out of a while or for that has an else, the else part #
-# does not execute. Execute the following code to see that the else clause    #
-# executes only if the break statement does not:                              #
-#                                                                             #
-#       for i in range(2):                                                    #
-#           value = int(input('Enter an integer (-1 to break): '))            #
-#           print('You entered:', value)                                      #
-#                                                                             #
-#           if value == -1:                                                   #
-#               break                                                         # 
-#       else:                                                                 #
-#           print('The loop terminated without executing the break')          #
-#                                                                             #
-###############################################################################
+'''
+The while and for statements each have an optional else clause. In a while
+statement, the else clause executes when the condition becomes False. In a
+for statement, the else clause executes when there are no more items to process.
+If you break out of a while or for that has an else, the else part does not
+execute. Execute the following code to see that the else clause executes only if
+the break statement does not
+                                                                            
+    for i in range(2):
+        value = int(input('Enter an integer (-1 to break): '))
+            print('You entered:', value
+
+        if value == -1:
+            break
+    else:
+        print('The loop terminated without executing the break')
+'''
 
 for i in range(2):
     value = int(input('Enter an integer (-1 to break): '))
@@ -26,4 +22,4 @@ for i in range(2):
     if value == -1:
         break
 else:
-    print('The loop terminated without executing the break')   
+    print('The loop terminated without executing the break')
